9a6fb1e5635fd8d75179498e798eac2fe694e37c
Joshua Wong <joshwonghc@gmail.com>
2019-06-18 23:56:36 +0800

    Finalized on using Blackfriday as md parser

--- parser.go
+++ parser.go
@@ -3,11 +3,33 @@ package golangmdtty
 import (
 	"bufio"
 	"fmt"
+	"strings"
 )
 
 // Parser is the object we use to parse the file and output it
 type Parser struct {
 	scanner bufio.Scanner
+}
+
+// We define a markdown block as having two EOF characters in a row (one line spacing)
+func splitFileFunction(data []byte, atEOF bool) (advanceBy int, block []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+
+	if atEOF {
+		return len(data), data, nil
+	}
+
+	index := strings.Index(string(data), "\n\n")
+	if index != -1 {
+		return index + 1, data[0:index], nil
+	}
+	return
+}
+
+func (p *Parser) setScannerSplit() {
+	p.scanner.Split(splitFileFunction)
 }
 
 func (p *Parser) getNextLine() {
